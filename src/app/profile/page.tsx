69b492c834707b6d8c4e1d19514e0d1648990e5b
MohammedMateenuddin <mateenuddin987@gmail.com>
2026-05-02 10:21:33 +0530

    fix: restore use client directive to top of files to fix black screen crash

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,5 +1,5 @@
-import Image from "next/image";
 "use client";
+import Image from "next/image";
 import React, { useState, useEffect } from "react";
 import { useRouter } from "next/navigation";
 import { supabase } from "@/lib/supabase/client";
